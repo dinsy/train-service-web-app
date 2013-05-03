847114566158fc36f0e0f89811d8a238342e301e
dinsy <longchengliang@gmail.com>
2013-05-03 12:47:46 +0000

    1,0

---
+++ source/package.js
@@ -0,0 +1,7 @@
+enyo.depends(
+	"$lib/layout/package.js",
+	"$lib/onyx/package.js",	// To theme Onyx using Theme.less, change this line to $lib/onyx/source,
+	//"Theme.less",	// uncomment this line, and follow the steps described in Theme.less
+	"App.css",
+	"App.js"
+);
